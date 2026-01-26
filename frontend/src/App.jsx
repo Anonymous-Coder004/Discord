0397750bc1ab14c31cdba23a86e5c75a14b65ba2
Yash_Srivastava <yash13sri@gmail.com>
2026-01-26 12:35:52 +0530

    folder structure defind

---
+++ frontend/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import 'tailwindcss'
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <h1 className='text-gray-700 dark:text-gray-400 '> Hello world</h1>
+    </>
+  )
+}
+
+export default App
