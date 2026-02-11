b735d93d32df87bad3a1acce64310d42aa0cdb1d
Yash_Srivastava <yash13sri@gmail.com>
2026-02-12 01:42:37 +0530

    feat:env structure

--- backend/app/core/config.py
+++ backend/app/core/config.py
@@ -1,16 +1,17 @@
-from pydantic_settings import BaseSettings
+from pydantic_settings import BaseSettings, SettingsConfigDict
+from pydantic import Field
 
-class Settings(BaseSettings): #pydantic model for env variable
-    secret_key:str
-    algorithm:str
-    access_token_expire_minutes:int
-    google_api_key:str
-    github_app_id:int
-    github_app_private_key:str
-    github_repository:str
-    huggingfacehub_api_token:str
-    database_url:str
-    class Config:
-        env_file=".env"
-    
+class Settings(BaseSettings):
+    secret_key: str = Field(..., alias="SECRET_KEY")
+    algorithm: str = Field(..., alias="ALGORITHM")
+    access_token_expire_minutes: int = Field(..., alias="ACCESS_TOKEN_EXPIRE_MINUTES")
+    google_api_key: str = Field(..., alias="GOOGLE_API_KEY")
+    github_app_id: int = Field(..., alias="GITHUB_APP_ID")
+    github_app_private_key: str = Field(..., alias="GITHUB_APP_PRIVATE_KEY")
+    github_repository: str = Field(..., alias="GITHUB_REPOSITORY")
+    huggingfacehub_api_token: str = Field(..., alias="HUGGINGFACEHUB_API_TOKEN")
+    database_url: str = Field(..., alias="DATABASE_URL")
+
+    model_config = SettingsConfigDict(env_file=".env")
+
 settings=Settings()
